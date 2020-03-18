6556623c2ae4b933fa2041397e112714b051eca2
Jordan Baucke <jordan.baucke@gmail.com>
2020-03-17 22:05:36 -0600

    updating meeting-map

--- meetup-map/MeetupMap.js
+++ meetup-map/MeetupMap.js
@@ -6,18 +6,59 @@ import './MeetupMap.css'
 class MeetupMap extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {}
+		this.state = {
+			width: 400,
+			pins: [
+				{
+					top: '240px',
+					left: '25px'
+				},
+				{
+					top: '575px',
+					left: '200px'
+				}
+			]
+		}
+	}
+
+	/**
+	 * Calculate & Update state of new dimensions
+	 */
+	updateDimensions() {
+		// if (window.innerWidth < 500) {
+		// 	this.setState({ width: 450, height: 102 })
+		// } else {
+		// 	let update_width = window.innerWidth - 100
+		// 	let update_height = Math.round(update_width / 4.4)
+		// 	this.setState({ width: update_width, height: update_height })
+		// }
+	}
+
+	/**
+	 * Add event listener
+	 */
+	componentDidMount() {
+		this.updateDimensions()
+		window.addEventListener('resize', this.updateDimensions.bind(this))
+	}
+
+	/**
+	 * Remove event listener
+	 */
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.updateDimensions.bind(this))
 	}
 
 	render() {
 		return (
-			<div className="map-container">
-				<img src={californiaMap} alt="California Map" />
-				<div style={{ position: 'absolute', top: '50vh' }}>
-					<img src={location} alt="Location Tag" />
-				</div>
-				<div style={{ position: 'absolute', top: '50px' }}>
-					<img src={location} alt="Location Tag" />
+			<div className="map-wrapper">
+				<div className="map-container" style={{ width: this.state.width }}>
+					<img src={californiaMap} alt="California Map" />
+					{this.state.pins.map(pin => (
+						<div style={{ position: 'absolute', top: pin.top, left: pin.left }}>
+							<img src={location} alt="Location Tag" />
+						</div>
+					))}
 				</div>
 			</div>
 		)
